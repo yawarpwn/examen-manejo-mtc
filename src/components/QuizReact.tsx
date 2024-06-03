94549ff26f9bcf150d8ac4010e4cce26d8b3159c
yawarpwn <john.kazz@gmail.com>
2024-06-02 23:22:33 -0500

    implent Quiz interative

--- src/components/QuizReact.tsx
+++ src/components/QuizReact.tsx
@@ -1,104 +1,94 @@
 import React from 'react'
+import { getQuestions } from '@/data/questions'
 import { useState } from 'react'
 import { cn } from '@/lib/utils'
 
-export function Quiz() {
-  const [selectId, setSelectId] = useState<number | null>(null)
+const questions = getQuestions()
 
-  const questions = [
-    {
-      id: 1,
-      text: 'No lo sé, espero descubrirlo con este test.',
-      value: 'a1',
-      letter: 'A',
-    },
-    {
-      id: 2,
-      text: 'otra pregunta de prueba',
-      value: 'a2',
-      letter: 'B',
-    },
+const getImageUrl = (id: number) =>
+  `https://sierdgtt.mtc.gob.pe/Content/img-data/img${id}.jpg`
 
-    {
-      id: 3,
-      text: 'No lo sé, espero descubrirlo con este test.',
-      value: 'a1',
-      letter: 'C',
-    },
-    {
-      id: 4,
-      text: 'otra pregunta de prueba',
-      value: 'a2',
-      letter: 'D',
-    },
+function getRandomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min)
+  const maxFloored = Math.floor(max)
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
 
-    {
-      id: 5,
-      text: 'otra pregunta de prueba',
-      value: 'a2',
-      letter: 'E',
-    },
-  ]
+export function QuizReact() {
+  const [randomIndex, setRandomIndex] = useState(getRandomInt(0, 200))
 
-  const handleClick = (id: number) => {
-    setSelectId(id)
+  const [selectedAlternative, setSelectedAlternative] = useState<string | null>(
+    null,
+  )
+
+  const question = questions[randomIndex]
+
+  const handleClick = (key: string) => {
+    setSelectedAlternative(key)
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    setSelectedAlternative(null)
+    setRandomIndex(getRandomInt(0, 200))
   }
+
+  const alternatives = Object.entries(question.alternatives)
 
   return (
     <form
       onSubmit={handleSubmit}
       className='mx-auto h-full w-full max-w-3xl p-4 xl:max-w-7xl'
     >
-      {JSON.stringify({
-        selectId,
-      })}
-      <div className='flex h-full flex-col justify-start gap-10 xl:flex-row xl:gap-16'>
-        <header className='flex flex-wrap items-center justify-center'>
+      {/* {JSON.stringify({ */}
+      {/*   selectId: selectedAlternative, */}
+      {/*   total: questions.length, */}
+      {/* })} */}
+      <div className='flex h-full flex-col justify-start gap-6 xl:flex-row xl:gap-16'>
+        <header className='flex flex-col flex-wrap items-center justify-center'>
           <div className='flex flex-col items-start justify-start'>
             <div className='spacing leading-6 tracking-wide text-white'>
-              <span>
-                {' '}
-                Antes de empezar, ¿cuál crees que es tu nivel de inglés?{' '}
-              </span>
+              <span>{question.question}</span>
             </div>
+          </div>
+          <div className='mt-4'>
+            {question.hasImage && <img src={getImageUrl(question.id)} />}
           </div>
         </header>
         <div className='flex h-full w-full flex-col items-center justify-center gap-6 xl:items-center'>
           <div className='w-full'>
             <div className='flex flex-col gap-4'>
-              {questions.map((question) => {
-                const checked = selectId === question.id
+              {alternatives.map(([alternative, value]) => {
+                const checked = selectedAlternative === alternative
 
                 return (
                   <label
                     data-state={checked ? 'checked' : 'unchecked'}
+                    key={alternative}
                     className={`relative flex cursor-pointer items-center overflow-hidden rounded-lg border border-zinc-500 bg-black pl-8 leading-6 data-[state=checked]:opacity-100 data-[state=unchecked]:opacity-30`}
                   >
                     <button
                       role='radio'
                       id='radio-button'
                       className='absolute bottom-0 left-0 top-0 w-8 min-w-8 bg-[#222] text-[#fff]'
-                      onClick={() => handleClick(question.id)}
+                      onClick={() => handleClick(alternative)}
                       type='button'
                       aria-checked={checked}
                       data-state='unchecked'
-                      value={question.value}
+                      value={alternative}
                     >
-                      {question.letter}
+                      {alternative}
                     </button>
                     <input
                       type='radio'
                       aria-hidden='true'
-                      value={question.value}
+                      value={value}
                       tabIndex={-1}
                       checked={checked}
-                      className='absolute h-14 w-8 -translate-x-full opacity-100'
+                      className='absolute h-14 w-8 -translate-x-full opacity-0'
                     />
-                    <span className='p-4'>{question.text}</span>
+                    <span className='p-4'>{value}</span>
                   </label>
                 )
               })}
@@ -110,9 +100,9 @@ export function Quiz() {
           <div className='mx-auto flex h-full max-w-3xl items-center justify-end rounded-tl-lg rounded-tr-lg bg-[#1e2229] p-4'>
             <button
               className='h-full w-full cursor-pointer rounded-md bg-lime-500 px-4 py-2 text-black disabled:pointer-events-none disabled:opacity-20'
-              aria-disabled={selectId == null}
+              aria-disabled={selectedAlternative == null}
               type='submit'
-              disabled={selectId == null}
+              disabled={selectedAlternative == null}
             >
               Siguiente
             </button>
---
+++ src/data/questions.ts
@@ -0,0 +1,37 @@
+import a1Questions from '@/data/a1.json'
+
+type Question = {
+  id: number
+  alternatives: {
+    a: string
+    b: string
+    c: string
+    d: string
+  }
+  question: string
+  correctAnswer: string
+  hasImage: boolean
+}
+
+const replaceMultipleSpaceWithSllipsis = (str: string) =>
+  str.replace(/_/, '').replace(/\s{2,}/g, ' ________ ')
+const format = (str: string) => str.slice(3, str.length)
+
+export function getQuestions(): Question[] {
+  const mappedQuestions = a1Questions.map((question) => {
+    return {
+      id: question.index,
+      alternatives: {
+        a: format(question.alternativa_a),
+        b: format(question.alternativa_b),
+        c: format(question.alternativa_c),
+        d: format(question.alternativa_d),
+      },
+      question: replaceMultipleSpaceWithSllipsis(question.pregunta),
+      correctAnswer: question.respuesta,
+      hasImage: Boolean(question.image),
+    }
+  })
+
+  return mappedQuestions
+}
